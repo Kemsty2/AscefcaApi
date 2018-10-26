e6890670d60e056a3575b2e1e44ebba8cb4c7bdb
= <kemsty2@yahoo.fr>
2018-10-26 13:45:13 +0100

    "Initialisation de l'api"

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,90 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 
-router.post("/api/signIn", (req, res) => {
+/*Route Okay*/
+router.post("/api/signIn", async (req, res) => {
   try{
-      
+    let userToSave, oldUser;
+    //On vérifie que le telephone n'est pas déja utilisé
+    oldUser = await Users.findOne({ where: {telephone: req.body.telephone} });
+    if(oldUser){
+      return res.json({error_message: 0, status: 0, message: "The Phone Already Exist"});
+    }else{
+      userToSave = await Users.create({
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        password: req.body.password,
+        telephone: req.body.telephone,
+        email: req.body.email,
+        status: req.body.status,
+        poids: req.body.poids,
+        taille: req.body.taille,
+        region_residence: req.body.region_residence,
+        activite: req.body.activite,
+        ville: req.body.ville,
+        GroupUserId: req.body.GroupUserId
+      });
+      return res.json({status: 1, uid: userToSave.id});
+    }
+  }catch (e) {
+    console.error(e);
+    res.json({error_message: 0, status:0});
+  }
+});
+
+/*Route Okay*/
+router.get("/api/login", async(req, res) => {
+  try {
+    let {telephone, password} = req.body, user;
+
+    user = await Users.findOne({where: {telephone: telephone, password: password}});
+    if(user){
+      return res.json({status: 1, user: user});
+    }else{
+      return res.json({status: 0, message: "The Password Or Mobile is not correct"});
+    }
+  }catch (e) {
+    console.error(e);
+    return res.json({status: 0, message: "The Password Or Mobile is not correct"});
+  }
+});
+
+router.get("/api/listfood", (req, res) => {
+  try {
+    
   }catch (e) {
     console.error(e);
     res.json(e);
   }
 });
+
+router.post("/api/rationIndividuelle", (req, res) => {
+  try {
+
+  }catch (e) {
+    console.error(e);
+    res.json(e);
+  }
+});
+
+router.get("/api/hygieneAlimentaire", (req, res) => {
+  try {
+
+  }catch (e) {
+    console.error(e);
+    res.json(e);
+  }
+});
+
+router.get("/api/calendarfood", (req, res) => {
+  try {
+
+  }catch (e) {
+    console.error(e);
+    res.json(e);
+  }
+});
+
+
 
 module.exports = router;
